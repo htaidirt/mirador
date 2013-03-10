21c5bd09fe05dacebcc101c6ac6038203b688fef
Hassen Taidirt <htaidirt@gmail.com>
2013-03-10 18:17:42 +0100

    Minimum locally working module.

---
+++ index.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+var test = require('./lib/test')
+,   watch = require('watch')
+,   cwd = process.cwd()
+
+console.log('Current working directory: ' + cwd + '...')
+
+// 
+/*
+ * First, check if the test/ folder exists.
+ * This is a requirement to perform tests.
+ */
+if (test.folderExists()) {
+
+    console.log('Ok, test/ folder exists...')
+
+    test.run()
+
+    watch.watchTree('./', { ignoreDotFiles: true }, function(file, curr, prev){
+        if(typeof file == 'object' && prev === null && curr === null) {
+            // Finished walking the tree.
+        } else { //
+            // File changed, new file or file removed.
+            if(file.charAt(0) != '.') {
+                console.log('File ' + file + ' changed...')
+                test.run()
+            }
+        }
+    })
+
+} else {
+    console.log('test/ folder does\'t exist. Please create it in order to perform tests.')
+}
